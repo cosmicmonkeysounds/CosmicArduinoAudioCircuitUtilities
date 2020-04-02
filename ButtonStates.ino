15b90c51d50ffef6c230813fa8ee56a3880b346b
John Janigan-Mills <cosmicmonkeysounds@gmail.com>
2020-04-02 15:33:18 -0300

    Better debounce and structs fully working for now.

--- ButtonStates.ino
+++ ButtonStates.ino
@@ -3,19 +3,13 @@
 
 // Setup and stuff for various constants used in the calculations of switch timing
 //////////////////////////////////////////////////////////////////////////////////
-#define CHECK_MSEC      2 // Read hardware every 5 msec
-#define PRESS_MSEC     10 // Stable time before registering pressed
-#define RELEASE_MSEC  100 // Stable time before registering released
-#define PRESS_DELAY    10
-#define RELEASE_DELAY 200
-
-#define DEBOUNCE_DELAY 50
+#define DEBOUNCE_DELAY 40
 
 // Giving "numbers" to the physical pins 
 ////////////////////////////////////////
 #define LED1 11
 #define LED2 A0
-#define BTN1 A2
+#define BTN1 A4
 #define BTN2 A5
 
 // This runs when the board boots and gets everything else ready
@@ -34,11 +28,10 @@ void setup()
 struct Button
 {
   int pin; // physical pin number
-  bool pressed = false, changed = false, debounced = false, onOrOff = false; // stores the state of the button
-  unsigned long lastDebounceTime = millis(); // important for debounce timing
-  
-  void push(); 
-  void debounceSwitch();
+  bool reading = false, state = false, lastState = false, onOrOff = false; // stores the state of the button
+  unsigned long lastDebounceTime = 0; // important for debounce timing
+
+  void check();
 
   Button ( int pinNum )
   {
@@ -46,55 +39,24 @@ struct Button
   }
 };
 
-void Button::debounceSwitch()
+void Button::check()
 {
-  static long dly = RELEASE_MSEC;
-  byte RawState;
-  
-  this->changed = false;
-  this->pressed = this->debounced;
-  
-  RawState = !digitalRead(this->pin);
-  if ( RawState == this->debounced )
+  this->reading = digitalRead(this->pin);
+  if ( this->reading != this->lastState )
   {
-    if ( this->debounced ) {
-      dly = RELEASE_DELAY;
-    } else {
-      dly = PRESS_DELAY;
-    }
+    lastDebounceTime = millis();
   }
-  else
+
+  if ( (millis() - this->lastDebounceTime) > DEBOUNCE_DELAY )
   {
-    if( (millis() - this->lastDebounceTime) > dly ) 
+    if ( this->reading != this->state )
     {
-      this->debounced = RawState;
-      this->changed = true;
-      this->pressed = this->debounced;
+      this->state = this->reading;
 
-      if ( this->debounced )
+      if ( this->state == HIGH )
       {
-        dly = RELEASE_DELAY;
-      } 
-      else 
-      {
-        dly = PRESS_DELAY;
+        this->onOrOff = !this->onOrOff;
       }
-      
-    } 
-  }
-}
-
-void Button::push()
-{
-  this->debounceSwitch();
-  if ( this->changed ) 
-  {
-    if ( this->pressed ) {
-      Serial.println("Key up");
-    } else {
-      Serial.println("Key down");
-      this->onOrOff = !this->onOrOff;
-      digitalWrite(LED2, this->onOrOff);
     }
   }
 }
@@ -104,10 +66,10 @@ struct State
   bool firstState = false, secondState = false;
   unsigned int numButtons;
   std::vector<Button> buttons;
-  int btnPins[2] = {A4, A5};
-  int ledPins[2] = {A0, 11};
+  int btnPins[2] = {BTN1, BTN2};
+  int ledPins[2] = {LED1, LED2};
   
-  void buttonPressed(int buttonNum);
+  void scan();
   
   State()
   {
@@ -119,15 +81,17 @@ struct State
   }
 } state;
 
-void State::buttonPressed(int buttonNum)
+void State::scan()
 {
-
+  for ( int i = 0; i < this->numButtons; ++i )
+    {
+      this->buttons.at(i).check();
+      digitalWrite( ledPins[i], this->buttons.at(i).onOrOff );
+      this->buttons.at(i).lastState = this->buttons.at(i).reading;
+    }
 }
 
 void loop() 
 {
-  for ( int i = 0; i < state.numButtons; ++i )
-  {
-    state.buttons.at(i).push();
-  }
+  state.scan();
 }
